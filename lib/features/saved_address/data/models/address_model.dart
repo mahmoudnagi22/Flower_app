267441e75180fbf5ad2a60c070e814c6c969585f
khorshed <mg6914949@gmail.com>
2025-04-24 17:14:15 +0200

    fix: update on address model

--- lib/features/saved_address/data/models/address_model.dart
+++ lib/features/saved_address/data/models/address_model.dart
@@ -1,43 +1,34 @@
-class AddressModel {
-  final String id;
-  final String street;
-  final String phone;
-  final String city;
-  final String lat;
-  final String long;
-  final String username;
+import 'package:flower_app/features/saved_address/domain/entities/address_entity.dart';
 
+class AddressModel extends AddressEntity {
   AddressModel({
-    required this.id,
-    required this.street,
-    required this.phone,
-    required this.city,
-    required this.lat,
-    required this.long,
-    required this.username,
+    required super.id,
+    required super.address,
+    required super.phoneNumber,
+    required super.city,
+    required super.recipientName,
+    required super.area,
   });
 
   factory AddressModel.fromJson(Map<String, dynamic> json) {
     return AddressModel(
       id: json['_id'],
-      street: json['street'],
-      phone: json['phone'],
+      address: json['address'],
+      phoneNumber: json['phoneNumber'],
+      recipientName: json['recipientName'],
       city: json['city'],
-      lat: json['lat'],
-      long: json['long'],
-      username: json['username'],
+      area: json['area'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       '_id': id,
-      'street': street,
-      'phone': phone,
+      'address': address,
+      'phoneNumber': phoneNumber,
+      'recipientName': recipientName,
       'city': city,
-      'lat': lat,
-      'long': long,
-      'username': username,
+      'area': area,
     };
   }
 }
