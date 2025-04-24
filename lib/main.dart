0995aa97717dca00303a5fe9772c1cf02ef45b09
Abdelrhman-18 <abdelrhmanv61@gmail.com>
2025-04-24 12:22:56 +0200

    Fix master branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,26 @@
-import 'package:flower_app/core/routes_manager/route_generator.dart';
-import 'package:flower_app/core/routes_manager/routes.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-import 'core/di/di.dart';
-import 'features/auth/login/presentation/cubit/login_cubit.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  // Bloc.observer = AppBlocObserver();
-  configureDependencies();
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  runApp(const FlowerApp());
+}
+class FlowerApp extends StatelessWidget {
+  const FlowerApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => LoginCubit(),
-      child: ScreenUtilInit(
-        designSize: const Size(375, 812),
-        minTextAdapt: true,
-        splitScreenMode: true,
-
-
-        builder: (context, child) => MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: child,
-          onGenerateRoute: RouteGenerator.getRoute,
-          initialRoute: Routes.bottomNav,
-        ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Center(
+            child: Text("welcome to master branch"),
+          )
+        ],
       ),
     );
   }
 }
-
 //flutter packages pub run build_runner watch
 //flutter packages pub run build_runner build
