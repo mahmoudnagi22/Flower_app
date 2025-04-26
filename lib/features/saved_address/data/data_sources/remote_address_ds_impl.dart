0b7c91f44e5ad0a9c1bf029e98bbd22a560c7975
khorshed <mg6914949@gmail.com>
2025-04-26 14:42:08 +0300

    fix: change on address ds impl

--- lib/features/saved_address/data/data_sources/remote_address_ds_impl.dart
+++ lib/features/saved_address/data/data_sources/remote_address_ds_impl.dart
@@ -1,4 +1,5 @@
 import 'package:flower_app/core/api_manager/api_manager.dart';
+import 'package:flower_app/core/models/user_model.dart';
 import 'package:flower_app/features/saved_address/data/data_sources/remote_address_ds_contract.dart';
 import 'package:flower_app/features/saved_address/data/models/address_model.dart';
 import 'package:injectable/injectable.dart';
@@ -10,24 +11,40 @@ class RemoteAddressDsImpl implements RemoteAddressDsContract {
 
   @override
   Future<void> addAddress(AddressModel address) async {
-    await apiManager.postRequest('addresses', address.toJson());
+    await apiManager.patchRequest(
+      'addresses',
+      address.toJson(),
+      headers: {"Authorization": "Bearer ${UserModel.instance.token}"},
+    );
   }
 
   @override
   Future<void> deleteAddress(int addressId) async {
-    await apiManager.deleteRequest('addresses/$addressId');
+    await apiManager.deleteRequest(
+      'addresses/$addressId',
+      headers: {"Authorization": "Bearer ${UserModel.instance.token}"},
+    );
   }
 
   @override
   Future<List<AddressModel>> getAddresses() async {
-    final response = await apiManager.getRequest('addresses');
-    return (response!.data['address'] as List)
-        .map((e) => AddressModel.fromJson(e))
-        .toList();
+    final response = await apiManager.getRequest(
+      'addresses',
+      headers: {"Authorization": "Bearer ${UserModel.instance.token}"},
+    );
+    final rawData = response?.data['address'];
+    if (rawData == null || rawData is! List) {
+      return [];
+    }
+    return rawData.map((e) => AddressModel.fromJson(e)).toList();
   }
 
   @override
   Future<void> updateAddress(AddressModel address) async {
-    await apiManager.putRequest('addresses/${address.id}', address.toJson());
+    await apiManager.patchRequest(
+      'addresses/${address.id}',
+      address.toJson(),
+      headers: {"Authorization": "Bearer ${UserModel.instance.token}"},
+    );
   }
 }
