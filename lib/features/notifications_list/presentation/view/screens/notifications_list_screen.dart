1855e9516b1ccac10523cd6053997ffc5c83c94e
khorshed <mg6914949@gmail.com>
2025-05-02 20:14:30 +0300

    feat: integrate cubit with ui

--- lib/features/notifications_list/presentation/view/screens/notifications_list_screen.dart
+++ lib/features/notifications_list/presentation/view/screens/notifications_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flower_app/core/di/di.dart';
 import 'package:flower_app/features/notifications_list/presentation/view/widgets/notification_item.dart';
 import 'package:flower_app/features/notifications_list/presentation/view_model/notification_cubit.dart';
+import 'package:flower_app/features/notifications_list/presentation/view_model/notification_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -11,7 +12,7 @@ class NotificationsListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => getIt<NotificationCubit>(),
+      create: (context) => getIt<NotificationCubit>()..getAllNotifications(),
       child: Scaffold(
         appBar: AppBar(
           title: Text(
@@ -25,11 +26,24 @@ class NotificationsListScreen extends StatelessWidget {
             icon: const Icon(Icons.arrow_back_ios_rounded),
           ),
         ),
-        body: ListView.separated(
-          separatorBuilder: (context, index) => Divider(),
-          itemCount: 3,
-          itemBuilder: (context, index) {
-            return NotificationItem();
+        body: BlocBuilder<NotificationCubit, NotificationState>(
+          builder: (context, state) {
+            if (state is NotificationLoading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is NotificationError) {
+              return Center(child: Text(state.message));
+            } else if (state is NotificationSuccess) {
+              return ListView.separated(
+                separatorBuilder: (context, index) => Divider(),
+                itemCount: state.notifications.length,
+                itemBuilder: (context, index) {
+                  return NotificationItem(
+                    notification: state.notifications[index],
+                  );
+                },
+              );
+            }
+            return SizedBox();
           },
         ),
       ),
--- lib/features/notifications_list/presentation/view/widgets/notification_item.dart
+++ lib/features/notifications_list/presentation/view/widgets/notification_item.dart
@@ -1,14 +1,20 @@
+import 'package:flower_app/features/notifications_list/domain/entities/notification_entity.dart';
 import 'package:flutter/material.dart';
 
 class NotificationItem extends StatelessWidget {
-  const NotificationItem({super.key});
+  const NotificationItem({super.key, required this.notification});
+  final NotificationEntity notification;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       leading: Icon(Icons.notifications_none),
-      title: Text('New Message'),
-      subtitle: Text('jskchjqscbhaicbujdbcjbc'),
+      title: Text(notification.title),
+      subtitle: Text(
+        notification.body,
+        style: TextStyle(overflow: TextOverflow.ellipsis),
+        maxLines: 2,
+      ),
     );
   }
 }
