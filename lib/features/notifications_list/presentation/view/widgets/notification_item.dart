f82b46abee8139112951bdb48e568f8b900146aa
khorshed <mg6914949@gmail.com>
2025-05-02 20:53:36 +0300

    fix: use slidable in notification item

--- lib/features/notifications_list/presentation/view/widgets/notification_item.dart
+++ lib/features/notifications_list/presentation/view/widgets/notification_item.dart
@@ -1,5 +1,6 @@
 import 'package:flower_app/features/notifications_list/domain/entities/notification_entity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class NotificationItem extends StatelessWidget {
   const NotificationItem({super.key, required this.notification});
@@ -7,13 +8,31 @@ class NotificationItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Icon(Icons.notifications_none),
-      title: Text(notification.title),
-      subtitle: Text(
-        notification.body,
-        style: TextStyle(overflow: TextOverflow.ellipsis),
-        maxLines: 2,
+    return Slidable(
+      startActionPane: ActionPane(
+        motion: const DrawerMotion(),
+        extentRatio: .44,
+        children: [
+          SlidableAction(
+            onPressed: (context) {
+              // deleteTask(task.id);
+            },
+            backgroundColor: const Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+            icon: Icons.delete,
+            label: 'delete',
+          ),
+        ],
+      ),
+      child: ListTile(
+        leading: Icon(Icons.notifications_none),
+        title: Text(notification.title),
+        subtitle: Text(
+          notification.body,
+          style: TextStyle(overflow: TextOverflow.ellipsis),
+          maxLines: 2,
+        ),
       ),
     );
   }
