9c00dd623d02624c3de80631b15d45cd4f249549
mostafamagde <mostafamagde227@gmail.com>
2025-05-15 05:29:48 +0300

    merge dev

--- lib/features/edit_profile/data/repositories/profile_repo_impl.dart
+++ lib/features/edit_profile/data/repositories/profile_repo_impl.dart
@@ -1,15 +1,14 @@
 import 'package:flower_app/core/models/user_model.dart';
-import 'package:flower_app/features/edit_profile/data/data_sources/remote_profile_datasorce_contract.dart';
 import 'package:flower_app/features/edit_profile/domain/repositories/profile_repo_contract.dart';
 import 'package:injectable/injectable.dart';
 
 @Singleton(as: EditProfileRepoContract)
 class ProfileRepoImpl extends EditProfileRepoContract {
-  final RemoteProfileDatasorceContract remoteProfileDatasorceContract;
+  final EditProfileRepoContract remoteProfileDatasorceContract;
   ProfileRepoImpl(this.remoteProfileDatasorceContract);
 
   @override
   Future<UserModel> updateUserProfile(UserModel userModel, String token) async {
-    return remoteProfileDatasorceContract.updateProfile(userModel, token);
+    return remoteProfileDatasorceContract.updateUserProfile(userModel, token);
   }
 }
